2cdde8ea7a33bcd130901364c15685f868d268c4
alkeshnagar56 <alkeshnagar56@gmail.com>
2024-11-29 15:42:24 +0530

    useeffect assignment to read the screen size

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,9 @@ useEffect(()=>{
 })
   return (
     <>
- <p>The actual size of the window:</p>
- <h1>{widthCount}</h1>
+    <p>This is made by using useEffect, to see the changes please inspect, and apply mobile view</p>
+    <p>The actual size of the window:</p>
+    <h1>{widthCount}</h1>
     </>
   )
 }
